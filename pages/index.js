7e3410d1184aefc24a73006a8d8ae860fd8cd5ff
Jordlolk <jordeallefsilvadecarvalho@gmail.com>
2025-06-15 16:48:02 -0300

    test git commands

--- pages/index.js
+++ pages/index.js
@@ -3,10 +3,8 @@ function HomeTemplate(){
         <>
         <section className="flex flex-col items-center justify-center">
             <h1 className="text-4xl font-bold mb-4">Welcome to My Website</h1>
-            <p className="text-lg mb-8">This is a simple homepage built with React and Tailwind CSS.</p>
         </section>
         </>
-
     )
 }
 
